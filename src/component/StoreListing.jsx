2a514de055c74faf4ee22a293fcc5d51e79e5a20
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2023-06-08 23:23:27 -0700

    For hw

---
+++ src/component/StoreListing.jsx
@@ -0,0 +1,52 @@
+import { Container, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
+
+const StyledContainer = styled(Container)`
+    margin-top: 320px;
+    text-align: center;
+
+`
+const StyledRow = styled(Row)`
+    border-bottom: 1px solid #e8e9eb;
+    display: flex;
+    justify-content: space-evenly;
+
+`  
+
+const CircleDiv = styled.div`
+    width: 40px;
+    height: 40px;
+    background-color: ${ props => props.color || 'red'}
+    border: 1px solid red;
+    border-radius: 100%;
+`
+
+const HorizontalRow = styled(Row)`
+    flex-direction: row;
+`
+
+const stores =['Aldi', 'Ralphs', 'Noble', '7-11', 'WinCO',]
+
+const storeMap = stores.map((storeName, key) => {
+    return <Row key={key}>
+        <Col xs='.5'>
+            <CircleDiv/>
+        </Col>
+        <Col>
+            <h5 style={{fontWeight: 'bold'}}>
+                {storeName}
+            </h5>
+            <p>
+                Delievery And PickUp 
+            </p>
+        </Col>
+    </Row>
+})
+
+export default function StoreListing(){
+    return <StyledContainer>
+        <StyledRow>
+            {storeMap}
+        </StyledRow>
+    </StyledContainer>
+}
